c43f23d8409ef00c9b17241dacb10979659661b9
Juliana Sau <julienyange@gmail.com>
2024-02-28 19:44:42 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,21 @@
-# Using Puppet to install flask v2.1.0 from pip3
+# Using Puppet to install Flask v2.1.0 from pip3
 
 package { 'Flask':
   ensure   => '2.1.0',
   name     => 'flask',
   provider => 'pip3',
+  require  => [
+    File['pip3'],
+    Package['Werkzeug']
+  ]
+}
+file { 'pip3':
+  ensure => 'present',
+  path   => '/usr/bin/pip3'
+}
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  name     => 'Werkzeug',
+  provider => 'pip3',
+  require  => File['pip3']
 }
