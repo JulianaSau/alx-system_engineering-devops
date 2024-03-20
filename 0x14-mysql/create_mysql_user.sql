16e05576b66f9c8e87fe0d0bb0e4f096656dcdc8
Juliana Sau <julienyange@gmail.com>
2024-03-20 19:46:07 +0300

    Update create_mysql_user.sql

--- 0x14-mysql/create_mysql_user.sql
+++ 0x14-mysql/create_mysql_user.sql
@@ -1,18 +1,9 @@
 -- create_mysql_user.sql
 
--- Create MySQL user holberton_user on web-01
+-- Create MySQL user holberton_user on web-01 and web-02
 CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
-
--- If needed, add SSH public key to web-02
-
--- Create MySQL user holberton_user on web-02
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
-
--- Ensure SSH task #3 is completed on both servers
 
 -- Optionally, you may also reload the SSH service to apply changes (Assuming SSH service is being used)
 -- sudo systemctl reload ssh
