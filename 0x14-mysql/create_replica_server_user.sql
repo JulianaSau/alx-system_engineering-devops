a7e7d220fcf213920380ebaf3207a5478fbdc8e5
Juliana Sau <julienyange@gmail.com>
2024-03-20 19:44:46 +0300

    Create create_replica_server_user.sql

---
+++ 0x14-mysql/create_replica_server_user.sql
@@ -0,0 +1,8 @@
+-- Create the replica_user with appropriate permissions for replication
+CREATE USER 'replica_user'@'%' IDENTIFIED BY 'replicauserpassword';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+
+-- Grant SELECT privilege on mysql.user to holberton_user
+GRANT SELECT ON mysql.user TO 'holberton_user'@'%';
+
+-- mysql -u root -p < create_replica_user.sql
