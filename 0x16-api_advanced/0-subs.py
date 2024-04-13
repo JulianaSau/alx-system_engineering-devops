b08f70f024fc0bb5854eb73501c4130ea5d68989
Juliana Sau <julienyange@gmail.com>
2024-04-13 06:25:21 -0700

    add task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Module contains a function that makes an api call
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    Makes an api call to get the number of subscribers in a given subreddit
+    Args:
+        subreddit(str) - The name of the subreddit to check the no of subs
+    '''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'android:com.example.myredditapp:v1.2.3 (by /u/kemitche)'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
+        return 0
+
