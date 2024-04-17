999789262eab58b031c833e9b53b516b29f7bf2c
Juliana Sau <julienyange@gmail.com>
2024-04-17 20:21:52 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
-'''
-Module contains a function that makes an api call
-'''
+"""This script gets number of subscribers from a subreddit"""
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    '''
-    Makes an api call to get the number of subscribers in a given subreddit
-    Args:
-        subreddit(str) - The name of the subreddit to check the no of subs
-    '''
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'aUserAgent'}
-
+    headers = {"User-Agent": "aUserAgent"}
+    url = "https://www.reddit.com/r/{}/about.json"\
+        .format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    else:
+    status_code = response.status_code
+    if status_code != 200:
         return 0
+    else:
+        response_json = response.json().get('data')
+        return response_json.get('subscribers')
+
+
+# #!/usr/bin/python3
+# '''
+# Module contains a function that makes an api call
+# '''
+# import requests
+
+
+# def number_of_subscribers(subreddit):
+#     '''
+#     Makes an api call to get the number of subscribers in a given subreddit
+#     Args:
+#         subreddit(str) - The name of the subreddit to check the no of subs
+#     '''
+#     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+#     headers = {'User-Agent': 'aUserAgent'}
+
+#     response = requests.get(url, headers=headers, allow_redirects=False)
+
+#     if response.status_code == 200:
+#         return response.json().get('data').get('subscribers')
+#     else:
+#         return 0
